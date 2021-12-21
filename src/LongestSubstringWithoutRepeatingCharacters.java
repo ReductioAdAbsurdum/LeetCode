f51ab8a73ec0553aa8a88183e68b2de2ded0f9b4
SergioStankovik <moonburn.co@gmail.com>
2021-12-21 19:05:21 +0100

    first commit

---
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+
+public class LongestSubstringWithoutRepeatingCharacters
+{
+    public int lengthOfLongestSubstring(String s)
+    {
+        if(s == null)
+        {
+            return 0;
+        }
+        if(s.length() == 0)
+        {
+            return 0;
+        }
+        if(s.length() == 1)
+        {
+            return 1;
+        }
+
+        HashMap<Character,Integer> letters = new HashMap<>();
+        letters.put(s.charAt(0),0);
+
+        int start = 0;
+        int end = 1;
+        int longest = 1;
+
+        while (end < s.length())
+        {
+            char l = s.charAt(end);
+            if(letters.containsKey(l))
+            {
+                int newStartIndex = letters.get(l) + 1;
+                for (int i = start; i < newStartIndex; i++)
+                {
+                    letters.remove(s.charAt(i));
+                }
+                start = newStartIndex;
+                letters.put(l,end);
+                end++;
+            }
+            else
+            {
+                letters.put(l,end);
+                end++;
+                longest = Math.max(end - start, longest);
+
+            }
+        }
+
+        return longest;
+    }
+}
